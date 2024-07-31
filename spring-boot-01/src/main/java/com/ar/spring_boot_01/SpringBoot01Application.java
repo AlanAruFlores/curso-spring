19c44764b29130d099afcbe00d26031ec8326398
AlanAruFlores <alanaruquipa242@gmail.com>
2024-07-30 22:52:45 -0300

    devtools

--- spring-boot-01/src/main/java/com/ar/spring_boot_01/SpringBoot01Application.java
+++ spring-boot-01/src/main/java/com/ar/spring_boot_01/SpringBoot01Application.java
@@ -7,8 +7,8 @@ import org.springframework.context.annotation.ComponentScans;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
-@ComponentScan(basePackages={"com.ar.spring_boot_01.others"})
+//@EnableScheduling
+//@ComponentScan(basePackages={"com.ar.spring_boot_01.others"})
 public class SpringBoot01Application {
 
 	public static void main(String[] args) {
