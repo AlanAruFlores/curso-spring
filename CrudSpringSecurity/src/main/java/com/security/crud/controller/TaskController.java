4973278f6dbc0f0cba1310555355e87bcdfc15c8
AlanAruFlores <alanaruquipa242@gmail.com>
2025-01-14 17:25:50 -0300

    feat show/add task

--- CrudSpringSecurity/src/main/java/com/security/crud/controller/TaskController.java
+++ CrudSpringSecurity/src/main/java/com/security/crud/controller/TaskController.java
@@ -6,10 +6,15 @@ import com.security.crud.service.ITaskService;
 import com.security.crud.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -31,6 +36,32 @@ public class TaskController {
         List<Task> taskList = taskService.getAllTaskByUser(user);
         model.addAttribute("taskList", taskList);
         return "home";
+    }
+
+
+    @RequestMapping(value = "/create-task", method = RequestMethod.GET)
+    public String createTask(Model model){
+        model.addAttribute("task", new Task());
+        return "create-task";
+    }
+
+    @RequestMapping(value = "/create-task", method = RequestMethod.POST)
+    public String createTask(Model model, @ModelAttribute("task") Task task, RedirectAttributes redirectAttributes){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = this.userService.getUserByUserName(authentication.getName());
+
+        task.setUser(user);
+        taskService.createTask(task);
+
+        redirectAttributes.addFlashAttribute("flagCreate", true);
+        return "redirect:/task/home";
+    }
+
+    @RequestMapping(value = "/show-task/{id}", method = RequestMethod.GET)
+    public String showTask(Model model, @PathVariable("id") Long id){
+        Task task = taskService.getTaskById(id);
+        model.addAttribute("task", task);
+        return "show-task";
     }
 
 }
